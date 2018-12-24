1e7323f98275c3f8dd3b0e59db252ab5a5439c94
Jacob Lichty <jacob.lichty@cbc.ca>
2018-12-24 04:41:34 -0500

    Restore HTML5CanvasMouseInputSystem

---
+++ src/html5/systems/HTML5CanvasMouseInputSystem.ts
@@ -0,0 +1,35 @@
+import Dictionary from '../../framework/concretes/data-structures/Dictionary';
+import HTML5CanvasMouseInputComponent from '../components/HTML5CanvasMouseInputComponent';
+import { HTML5_CANVAS_MOUSE_INPUT_EVENT } from '../enums/HTML5_CANVAS_MOUSE_INPUT_EVENT';
+import IHTML5CanvasMouseInputCommand from '../interfaces/IHTML5CanvasMouseInputCommand';
+import InputSystem from '../../framework/abstracts/InputSystem';
+
+export default class HTML5CanvasMouseInputSystem extends InputSystem<HTML5CanvasMouseInputComponent> {
+
+    private __strategies: Dictionary<IHTML5CanvasMouseInputCommand>;
+
+    constructor() {
+        super(HTML5CanvasMouseInputComponent);
+        this.__strategies = new Dictionary<IHTML5CanvasMouseInputCommand>();
+    }
+
+    public set(event: HTML5_CANVAS_MOUSE_INPUT_EVENT, command: IHTML5CanvasMouseInputCommand): void {
+        this.__strategies.write({
+            key: event,
+            value: command,
+        });
+    }
+
+    public reset(): void {
+        this.__strategies.flush();
+    }
+
+    public once(component: HTML5CanvasMouseInputComponent): void {
+        const command = this.__strategies.read(component.data.eventName);
+        if (!command) {
+            throw new Error(`${this.constructor.name} has no matching input command!`);
+        }
+        command.invoke(component.data.cursor);
+    }
+
+}
