2a2a8cbcbf0d3cdc9ba25512d9f12217a7ef9d61
Jacob Lichty <jacob.lichty@cbc.ca>
2019-01-08 22:19:35 -0500

    remove unused framework stuff

--- src/framework/concretes/Command.ts
+++
@@ -1,7 +0,0 @@
-import ICommand from '../interfaces/ICommand';
-import Invocable from '../abstracts/Invocable';
-
-export default class Command<TPayload, TResult> extends Invocable<TPayload, TResult>
-implements ICommand<TPayload, TResult> {
-
-}
--- src/framework/interfaces/IQueue.ts
+++
@@ -1,9 +0,0 @@
-import { Optional } from '../types/Optional';
-
-export default interface IQueue<T> {
-
-    push(item: T): void;
-
-    pop(): Optional<T>;
-
-}
